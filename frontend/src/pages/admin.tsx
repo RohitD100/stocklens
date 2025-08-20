bdf1b92c64227add87fb09b3e407a8e5710c880c
rohit <rohitdorage3@gmail.com>
2025-08-20 11:18:54 +0530

    feat(frontend): add admin page

---
+++ frontend/src/pages/admin.tsx
@@ -0,0 +1,9 @@
+const admin = () => {
+  return (
+    <div className="flex h-[100vh] w-full items-center justify-center">
+      <h1 className="text-[25px]">Admin</h1>
+    </div>
+  );
+};
+
+export default admin;
