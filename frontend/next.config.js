4cc7fafe5f488abe32f4ad2fb395b36abb21d413
rohit <rohitdorage3@gmail.com>
2025-08-20 07:33:06 +0530

    chore(frontend): initial commit of frontend
    - Added .gitignore, .prettierignore, and .prettierrc.json
    - Created basic project structure and files
    - Added Next.js app files (pages, layout, globals)
    - Set up ESLint and Prettier configurations
    - Included basic README and package.json

---
+++ frontend/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    root: __dirname, // Explicitly set project root
+  },
+};
+
+module.exports = nextConfig;
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex h-[100vh] w-full items-center justify-center border border-amber-700">
+        Stock lense
+      </div>
+    </>
+  );
+}
